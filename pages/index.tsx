70f4fd0cd383d088a418d59bbf0810da6096247e
Arpit Bhalla <55053424+arpitBhalla@users.noreply.github.com>
2022-03-22 14:58:29 +0530

    add dashboard page

---
+++ pages/index.tsx
@@ -0,0 +1,24 @@
+import { useState, useEffect } from "react";
+import Dashboard from "@components/Clubs";
+// import Dashboard from "@components/Home";
+import { Header } from "@layouts";
+import { supabase } from "@utils/supabaseClient";
+import { Session } from "@supabase/supabase-js";
+import { basePath } from "site.config";
+import { useRouter } from "next/router";
+import { WithProtectedPage, useUser } from "@context/Auth";
+import dynamic from "next/dynamic";
+
+const S = dynamic(() => import("@components/Home"));
+
+function Home() {
+  return (
+    <>
+      <Header />
+      {/* <S /> */}
+      <Dashboard />
+    </>
+  );
+}
+
+export default WithProtectedPage(Home, ["admin", "member"]);
